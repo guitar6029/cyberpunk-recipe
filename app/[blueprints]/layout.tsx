b1d1a81f8c4de802d5704b6485b82a02324a0f50
Josh <jsbusiness305@gmail.com>
2025-01-18 23:15:00 -0500

    added prop cornersColor to the TemplateCorners

--- app/[blueprints]/layout.tsx
+++ app/[blueprints]/layout.tsx
@@ -2,7 +2,7 @@ import TemplateCorners from "@/components/Template/TemplateCorners";
 const Layout : React.FC = () => {
     return ( 
         <div className="min-h-screen flex flex-col p-4">
-            <TemplateCorners>
+            <TemplateCorners cornerColor="bg-green-500">
                 <div>hello</div>
             </TemplateCorners>
         </div>
--- components/Template/TemplateCorners.tsx
+++ components/Template/TemplateCorners.tsx
@@ -1,23 +1,28 @@
 import type { ReactNode } from 'react';
 
-const TemplateCorners: React.FC<{ children: ReactNode }> = ({ children }) => {
+interface TemplateCornersProps {
+    children: ReactNode;
+    cornerColor?: string;
+}
+
+const TemplateCorners: React.FC<TemplateCornersProps> = ({ children, cornerColor = 'bg-white' }) => {
     return (
         <div className="relative min-h-screen flex flex-col p-4">
             {/* top left corner */}
-            <div className="absolute top-0 left-0 bg-white w-[10px] h-[50px]"></div>
-            <div className="absolute -top-5 left-5 rotate-90 bg-white w-[10px] h-[50px]"></div>
+            <div className={`absolute top-0 left-0 ${cornerColor} w-[10px] h-[50px]`}></div>
+            <div className={`absolute -top-5 left-5 rotate-90 ${cornerColor} w-[10px] h-[50px]`}></div>
             
             {/* top right corner */}
-            <div className="absolute top-0 right-0 bg-white w-[10px] h-[50px]"></div>
-            <div className="absolute -top-5 right-5 rotate-90 bg-white w-[10px] h-[50px]"></div>
+            <div className={`absolute top-0 right-0 ${cornerColor} w-[10px] h-[50px]`}></div>
+            <div className={`absolute -top-5 right-5 rotate-90 ${cornerColor} w-[10px] h-[50px]`}></div>
             
             {/* bottom left corner */}
-            <div className="absolute bottom-0 left-0 bg-white w-[10px] h-[50px]"></div>
-            <div className="absolute -bottom-5 left-5 rotate-90 bg-white w-[10px] h-[50px]"></div>
+            <div className={`absolute bottom-0 left-0 ${cornerColor} w-[10px] h-[50px]`}></div>
+            <div className={`absolute -bottom-5 left-5 rotate-90 ${cornerColor} w-[10px] h-[50px]`}></div>
             
             {/* bottom right corner */}
-            <div className="absolute bottom-0 right-0 bg-white w-[10px] h-[50px]"></div>
-            <div className="absolute -bottom-5 right-5 rotate-90 bg-white w-[10px] h-[50px]"></div>
+            <div className={`absolute bottom-0 right-0 ${cornerColor} w-[10px] h-[50px]`}></div>
+            <div className={`absolute -bottom-5 right-5 rotate-90 ${cornerColor} w-[10px] h-[50px]`}></div>
 
             {children}
         </div>
