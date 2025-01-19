17f7b52c836b0f63c3e2ef516c89a95d6d1f1280
Josh <jsbusiness305@gmail.com>
2025-01-19 00:04:06 -0500

    added slider logic for biohacks cards

--- app/biohacks/page.tsx
+++ app/biohacks/page.tsx
@@ -1,24 +1,58 @@
+"use client";
+
 import HackCard from "@/components/Card/HackCard";
+import { useState } from 'react';
 import { gadgets, Gadget } from "@/dummydata/hacks";
 import Link from "next/link";
-import { Cpu } from 'lucide-react';
+import { Cpu, ChevronLeft, ChevronRight } from 'lucide-react';
+import TemplateCorners from "@/components/Template/TemplateCorners";
 
 const Biohacks = () => {
-    return (
-    <div className="min-h-screen flex flex-col gap-4 p-4">
-       <div className="flex flex-row items-center gap-2">
-        <h1 className="text-4xl text-yellow-200">Biohacks</h1>
-        <Cpu size={30} /> 
-        </div> 
-        <div className="flex flex-col items-center gap-4">
-            {gadgets && gadgets.map((gadget: Gadget) => (
-                <Link key={gadget.gadget} href={`/biohacks/${gadget.gadget}`} className="w-full" >
-                    <HackCard gadget={gadget} />
-                </Link>
-            ))}
-        </div>
 
-    </div>);
+    const [currentBiohack, setCurrentBiohack] = useState(0);
+
+    const handleChangeSlide = (direction: number) => {
+        if (direction === 1) {
+            let currentIndex = currentBiohack + 1 >= gadgets.length ? 0 : currentBiohack + 1;
+            setCurrentBiohack(currentIndex);
+        } else {
+            let currentIndex = currentBiohack - 1 < 0 ? gadgets.length - 1 : currentBiohack - 1;
+            setCurrentBiohack(currentIndex);
+        }
+    }
+
+    const previousIndex = currentBiohack - 1 < 0 ? gadgets.length - 1 : currentBiohack - 1;
+    const nextIndex = currentBiohack + 1 >= gadgets.length ? 0 : currentBiohack + 1;
+
+    return (
+        <TemplateCorners cornerColor="bg-green-500">
+            <div className="flex flex-grow items-center justify-between min-h-screen">
+                <ChevronLeft size={60} onClick={() => handleChangeSlide(-1)} className="text-green-500 cursor-pointer" />
+                
+                <div className="flex flex-row items-center gap-10">
+                    {/* Previous Slide */}
+                    <div className="flex flex-col gap-2 w-[250px] h-[100px] truncate rounded-lg shadow-lg shadow-stone-500 blur">
+                        <span>{gadgets[previousIndex].gadget}</span>
+                        <span>{gadgets[previousIndex].shortDescription}</span>
+                    </div>
+                    
+                    {/* Current Slide */}
+                    <div className="flex flex-col gap-2 w-[250px] h-[100px] truncate rounded-lg shadow-lg shadow-stone-500 scale-125">
+                        <span>{gadgets[currentBiohack].gadget}</span>
+                        <span>{gadgets[currentBiohack].shortDescription}</span>
+                    </div>
+
+                    {/* Next Slide */}
+                    <div className="flex flex-col gap-2 w-[250px] h-[100px] truncate rounded-lg shadow-lg shadow-stone-500 blur">
+                        <span>{gadgets[nextIndex].gadget}</span>
+                        <span>{gadgets[nextIndex].shortDescription}</span>
+                    </div>
+                </div>
+                
+                <ChevronRight size={60} onClick={() => handleChangeSlide(1)} className="text-green-500 cursor-pointer" />
+            </div>
+        </TemplateCorners>
+    );
 }
 
 export default Biohacks;
