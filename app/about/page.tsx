b730a16e2902a47f6d2c6cc69d7bb9b8fcb52d8a
Josh <jsbusiness305@gmail.com>
2025-01-20 20:17:57 -0500

    updated testimonials , and about page image size

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ const Page : React.FC = () => {
         <div className="min-h-screen flex flex-col p-4">
             <TemplateCorners cornerColor="bg-green-500">
                 <div className="flex flex-col gap-4 p-4">
-                    <Image quality={100}  className="w-[50rem]" width={500} height={500} src={'/biohacks/gadgets/gadget1.png'} alt="cyberpunk" />
+                    <Image quality={100}  className="w-[30rem] rounded-lg" width={500} height={500} src={'/biohacks/gadgets/gadget1.png'} alt="cyberpunk" />
                     <p className="sm:text-xl leading-relaxed lg:text-2xl text-gray-for-dark-drop">Welcome to &quot;hacks&quot;, a groundbreaking project set in a cyberpunk universe where users can unleash their creativity to build custom hacks, a groundbreaking project set in a cyberpunk universe where users can unleash their creativity to build custom hacks or discover free recipes to construct advanced gadgets. Inspired by the vibrant neon-lit streets of Cyberpunk 2077 and the rain-soaked, dystopian world of Blade Runner, Neon Nexus promises an immersive experience where technology and imagination merge seamlessly.</p>
                 </div>
             </TemplateCorners>
--- components/Testimonials/Testimonial.tsx/Testimonial.tsx
+++ components/Testimonials/Testimonial.tsx/Testimonial.tsx
@@ -1,64 +1,22 @@
 "use client";
 import { TTestimonial } from "@/dummydata/testimonials";
-import { useState, useEffect, useCallback } from "react";
 import Image from "next/image";
 
 type TestimonialProps = {
     person: TTestimonial
 }
 
-const ALIEN_CHARACTERS = ['*', '(', ')', '<', '>', ',', '.', '#', '$', '^', '!', '%', '|', '\\', '@', '&', '~', '{', '}', '[', ']', ':', ';', '?', '-', '+', '=', '_'];
-
-const generateAlienText = (text: string, setDisplay: React.Dispatch<React.SetStateAction<string[]>>, getAlienToOriginalText: (alienString: string[], originalString: string, setDisplay: React.Dispatch<React.SetStateAction<string[]>>) => void) => {
-    const characters = text.split('').map((char: string) => char);
-    const myAlienString = characters.map(() => {
-        const randomIndex = Math.floor(Math.random() * ALIEN_CHARACTERS.length);
-        return ALIEN_CHARACTERS[randomIndex];
-    });
-
-    setDisplay(myAlienString);
-
-    setTimeout(() => {
-        getAlienToOriginalText(myAlienString, text, setDisplay);
-    }, 3000); // 3-second delay before starting
-}
-
-const getAlienToOriginalText = (alienString: string[], originalString: string, setDisplay: React.Dispatch<React.SetStateAction<string[]>>) => {
-    alienString.forEach((_, index: number) => {
-        setTimeout(() => {
-            setDisplay((prev: string[]) => {
-                const newText = [...prev];
-                newText[index] = originalString[index];
-                return newText;
-            });
-        }, index * 20); // Adjust the timing as needed
-    });
-}
-
 const Testimonial: React.FC<TestimonialProps> = ({ person }: TestimonialProps) => {
-    
-    const [displayText, setDisplayText] = useState<string[]>([]);
-    const [displayedName, setDisplayedName] = useState<string[]>([]);
-
-    const memoizedGenerateAlienText = useCallback((text: string, setDisplay: React.Dispatch<React.SetStateAction<string[]>>) => {
-        generateAlienText(text, setDisplay, getAlienToOriginalText);
-    }, []);
-
-    useEffect(() => {
-        memoizedGenerateAlienText(person.description, setDisplayText);
-    }, [person.description, memoizedGenerateAlienText]);
-
-    useEffect(() => {
-        memoizedGenerateAlienText(person.name, setDisplayedName);
-    }, [person.name, memoizedGenerateAlienText]);
-
     return (
         <div className="relative flex flex-row items-center gap-10 rounded-lg p-4 w-fit h-fit">
             <div className="flex flex-col gap-12">
-
                 <div className="col-span-2 flex flex-col gap-2 p-4 rounded-lg xl:-skew-y-10 shadow-lg shadow-indigo-500">
-                    <q className="text-3xl font-bold text-gray-for-dark-drop text-left leading-relaxed p-4 min-h-[20rem]">{displayText}</q>
-                    <h1 className="text-2xl font-bold text-yellow-200 p-4">{displayedName}</h1>
+                    <q className="text-3xl font-bold text-gray-for-dark-drop text-left leading-relaxed p-4 min-h-[20rem]">
+                        {person.description}
+                    </q>
+                    <h1 className="text-2xl font-bold text-yellow-200 p-4">
+                        {person.name}
+                    </h1>
                     <div className="relative float-right flex flex-col items-end justify-end">
                         <div className="relative w-fit h-fit flex flex-col items-center justify-center float-right">
                             <div className="absolute w-[8rem] h-[8rem] md:w-[12rem] md:h-[12rem] rotate-[45deg] inset-0 border-4 border-slate-300 z-10"></div>
