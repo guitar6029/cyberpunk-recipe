6ca782f1ac36e1bf17c97efa9ba021df5fa72d34
Josh <jsbusiness305@gmail.com>
2025-02-22 17:24:45 -0500

    added cyber text to the header menu

--- components/Buttons/Nav/NavLink.tsx
+++ components/Buttons/Nav/NavLink.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 type NavLinkProps = {
     btnText: string;
     linktTo: string;
+    showLinkLayout?: boolean
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ btnText, linktTo }) => {
+const NavLink: React.FC<NavLinkProps> = ({ btnText, linktTo, showLinkLayout = true }) => {
     return (
         <div className="relative w-fit flex flex-col items-center justify-center cursor-pointer ">
-            <div className="absolute inset-0 bg-purple-500 z-10 link-layout"></div>
-            <Link className="z-20 hover:scale-105 transition duration-200 ease-in  p-4 sm:text-xl text-5xl font-bold cursor-pointer"  href={linktTo}>{btnText}</Link>
+            
+            {showLinkLayout && <div className="absolute inset-0 bg-purple-500 z-10 link-layout"></div>}
+            <Link className="z-20 hover:scale-105 transition duration-200 ease-in  p-4 sm:text-md lg:text-5xl font-bold cursor-pointer"  href={linktTo}>{btnText}</Link>
         </div>
 
     );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,21 @@
+import { alkeron } from "../public/fonts/fonts";
 import NavLink from "./Buttons/Nav/NavLink";
 const Navbar = () => {
-    return (
-        <div className="flex flex-row items-center gap-4 p-4 h-[100px] bg-black">
-
-           
-            <div className="flex flex-row items-center gap-2">
-
-                <NavLink linktTo="/" btnText="Home" />
-                <NavLink linktTo="/biohacks" btnText="Biohacks" />
-                <NavLink linktTo="/about" btnText="About" />
-
-
-            </div>
-
-
-        </div>);
-}
+  return (
+    <div
+      className={`flex flex-row items-center gap-4 p-4 h-[100px] bg-black ${alkeron.className}`}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <NavLink linktTo="/" btnText="Home" showLinkLayout={false} />
+        <NavLink
+          linktTo="/biohacks"
+          btnText="Biohacks"
+          showLinkLayout={false}
+        />
+        <NavLink linktTo="/about" btnText="About" showLinkLayout={false} />
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
