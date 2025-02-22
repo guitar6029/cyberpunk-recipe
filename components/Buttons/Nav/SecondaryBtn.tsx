c845b565eec8a4c36e306601bd24d3d4a0febba7
Josh <jsbusiness305@gmail.com>
2025-02-22 18:16:36 -0500

    updated footer text

--- components/Buttons/Nav/SecondaryBtn.tsx
+++ components/Buttons/Nav/SecondaryBtn.tsx
@@ -1,3 +1,4 @@
+import { alkeron } from "@/public/fonts/fonts";
 
 type BtnProps = {
     btnText: string;
@@ -7,7 +8,7 @@ const SecondaryBtn: React.FC<BtnProps> = ({ btnText }) => {
     return (
         <div className="relative w-fit flex flex-col items-center justify-center ">
             <div className="absolute inset-0 bg-gradient-to-r from-[#2980b9] via-[#9b59b6] to-[#7e50e9] z-10 link-layout"></div>
-            <button className="z-20 transition duration-200 ease-in  p-4 text-5xl font-bold">{btnText}</button>
+            <button className={`z-20 transition duration-200 ease-in  p-4 text-5xl font-bold ${alkeron.className}`}>{btnText}</button> 
         </div>
 
     );
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,32 +2,45 @@ import Link from "next/link";
 import SecondaryBtn from "../Buttons/Nav/SecondaryBtn";
 
 const Footer: React.FC = () => {
-    return (
-        <footer className="h-full sm:h-[25vh] flex sm:flex-col md:flex-row gap-2 items-center justify-around bg-black">
-            
-            <SecondaryBtn btnText="hacks &copy; 2077" />
-            
-            <div className="flex flex-row gap-10">
+  return (
+    <footer className="h-full sm:h-[25vh] flex sm:flex-col md:flex-row gap-2 items-center justify-around bg-black">
+      <div className="flex flex-row  items-center gap-2">
+        <SecondaryBtn btnText="hacks &copy;" />
+        <span>2077</span>
+      </div>
 
-                <div className="flex flex-col gap-3">
-                    <h1 className="font-bold text-2xl text-[#828085]">Category</h1>
-                    <Link href={"/#"} className="text-xl text-purple-400 hover:underline">Link 1</Link>
-                    <Link href={"/#"} className="text-xl text-purple-400 hover:underline">Link 2</Link>
-                </div>
+      <div className="flex flex-row gap-10">
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-2xl text-[#828085]">Category</h1>
+          <Link href={"/#"} className="text-xl text-purple-400 hover:underline">
+            Link 1
+          </Link>
+          <Link href={"/#"} className="text-xl text-purple-400 hover:underline">
+            Link 2
+          </Link>
+        </div>
 
-                <div className="flex flex-col gap-3">
-                    <h1 className="font-bold text-2xl text-[#828085]">Category</h1>
-                    <Link href={"/#"} className="text-xl text-purple-400 hover:underline">Link 3</Link>
-                    <Link href={"/#"} className="text-xl text-purple-400 hover:underline">Link 4</Link>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <h1 className="font-bold text-2xl text-[#828085]">Category</h1>
-                    <Link href={"/#"} className="text-xl text-purple-400 hover:underline">Link 3</Link>
-                    <Link href={"/#"} className="text-xl text-purple-400 hover:underline">Link 4</Link>
-                </div>
-            </div>
-        </footer>
-    );
-}
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-2xl text-[#828085]">Category</h1>
+          <Link href={"/#"} className="text-xl text-purple-400 hover:underline">
+            Link 3
+          </Link>
+          <Link href={"/#"} className="text-xl text-purple-400 hover:underline">
+            Link 4
+          </Link>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-2xl text-[#828085]">Category</h1>
+          <Link href={"/#"} className="text-xl text-purple-400 hover:underline">
+            Link 3
+          </Link>
+          <Link href={"/#"} className="text-xl text-purple-400 hover:underline">
+            Link 4
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
