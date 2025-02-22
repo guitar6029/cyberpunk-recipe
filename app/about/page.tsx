1eae6b4acdb3d0ebdf196571357c43fe716d4afc
Josh <jsbusiness305@gmail.com>
2025-02-22 17:41:41 -0500

    updated nav link class style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,12 @@
-import Image from "next/image";
+
 
 import TemplateCorners from "@/components/Template/TemplateCorners";
 const Page: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col p-4">
-      <TemplateCorners cornerColor="bg-green-500">
-        <div className="relative flex flex-col gap-4 p-4 items-center justify-center">
-          <Image
-            quality={100}
-            className="w-[50rem] rounded-lg absolute top-0 left-20 opacity-20 z-[-1]"
-            width={500}
-            height={500}
-            src={"/biohacks/gadgets/gadget1.png"}
-            alt="cyberpunk"
-          />
-          <p className="sm:text-xl sm:w-[100vw] lg:w-[40vw] leading-relaxed lg:text-2xl text-white bg-neutral-800 p-4 rounded-lg border-yellow-500 border-2">
+      <TemplateCorners cornerColor="bg-transparent">
+        <div className="relative flex flex-col gap-4 p-4">
+          <p className="sm:text-xl sm:w-[100vw] lg:w-[40vw] leading-relaxed lg:text-4xl text-gray-300  p-4 rounded-lg border-yellow-500 border-2">
             Welcome to &quot;hacks&quot;, a groundbreaking project set in a
             cyberpunk universe where users can unleash their creativity to build
             custom hacks, a groundbreaking project set in a cyberpunk universe
--- components/Buttons/Nav/NavLink.tsx
+++ components/Buttons/Nav/NavLink.tsx
@@ -11,7 +11,7 @@ const NavLink: React.FC<NavLinkProps> = ({ btnText, linktTo, showLinkLayout = tr
         <div className="relative w-fit flex flex-col items-center justify-center cursor-pointer ">
             
             {showLinkLayout && <div className="absolute inset-0 bg-purple-500 z-10 link-layout"></div>}
-            <Link className="z-20 hover:scale-105 transition duration-200 ease-in  p-4 sm:text-md lg:text-5xl font-bold cursor-pointer"  href={linktTo}>{btnText}</Link>
+            <Link className="z-1 p-4 sm:text-md lg:text-5xl font-bold cursor-pointer nav-link"  href={linktTo}>{btnText}</Link>
         </div>
 
     );
