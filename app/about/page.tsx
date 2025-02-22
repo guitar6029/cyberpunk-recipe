cb8dbf0c0b3445179038cdfb48eabd7661494169
Josh <jsbusiness305@gmail.com>
2025-02-22 17:46:39 -0500

    added boolean prop for corners ui toggle

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,8 @@
-
-
 import TemplateCorners from "@/components/Template/TemplateCorners";
 const Page: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col p-4">
-      <TemplateCorners cornerColor="bg-transparent">
+      <TemplateCorners showCorners={false}>
         <div className="relative flex flex-col gap-4 p-4">
           <p className="sm:text-xl sm:w-[100vw] lg:w-[40vw] leading-relaxed lg:text-4xl text-gray-300  p-4 rounded-lg border-yellow-500 border-2">
             Welcome to &quot;hacks&quot;, a groundbreaking project set in a
--- components/Template/TemplateCorners.tsx
+++ components/Template/TemplateCorners.tsx
@@ -1,33 +1,75 @@
-import type { ReactNode } from 'react';
+import type { ReactNode } from "react";
 
 interface TemplateCornersProps {
-    children: ReactNode;
-    cornerColor?: string;
-    minHScreen?: boolean
+  children: ReactNode;
+  cornerColor?: string;
+  minHScreen?: boolean;
+  showCorners?: boolean;
 }
 
-const TemplateCorners: React.FC<TemplateCornersProps> = ({ children, cornerColor = 'bg-white', minHScreen = true }) => {
-    return (
-        <div className={`relative ${minHScreen ? 'min-h-screen' : 'h-fit' } flex flex-col p-4`}>
-            {/* top left corner */}
-            <div className={`absolute top-0 left-0 ${cornerColor} w-[5px] h-[50px]`}></div>
-            <div className={`absolute -top-6 left-6 rotate-90 ${cornerColor} w-[5px] h-[50px]`}></div>
-            
-            {/* top right corner */}
-            <div className={`absolute top-0 right-0 ${cornerColor} w-[5px] h-[50px]`}></div>
-            <div className={`absolute -top-6 right-6 rotate-90 ${cornerColor} w-[5px] h-[50px]`}></div>
-            
-            {/* bottom left corner */}
-            <div className={`absolute bottom-0 left-0 ${cornerColor} w-[5px] h-[50px]`}></div>
-            <div className={`absolute -bottom-6 left-6 rotate-90 ${cornerColor} w-[5px] h-[50px]`}></div>
-            
-            {/* bottom right corner */}
-            <div className={`absolute bottom-0 right-0 ${cornerColor} w-[5px] h-[50px]`}></div>
-            <div className={`absolute -bottom-6 right-6 rotate-90 ${cornerColor} w-[5px] h-[50px]`}></div>
+const TemplateCorners: React.FC<TemplateCornersProps> = ({
+  children,
+  cornerColor = "bg-white",
+  minHScreen = true,
+  showCorners = true,
+}) => {
+  return (
+    <div
+      className={`relative ${
+        minHScreen ? "min-h-screen" : "h-fit"
+      } flex flex-col p-4`}
+    >
+      {/* top left corner */}
+      {showCorners && (
+        <>
+          <div
+            className={`absolute top-0 left-0 ${cornerColor} w-[5px] h-[50px]`}
+          ></div>
+          <div
+            className={`absolute -top-6 left-6 rotate-90 ${cornerColor} w-[5px] h-[50px]`}
+          ></div>
+        </>
+      )}
 
-            {children}
-        </div>
-    );
-}
+      {/* top right corner */}
+      {showCorners && (
+        <>
+          <div
+            className={`absolute top-0 right-0 ${cornerColor} w-[5px] h-[50px]`}
+          ></div>
+          <div
+            className={`absolute -top-6 right-6 rotate-90 ${cornerColor} w-[5px] h-[50px]`}
+          ></div>
+        </>
+      )}
+
+      {/* bottom left corner */}
+      {showCorners && (
+        <>
+          <div
+            className={`absolute bottom-0 left-0 ${cornerColor} w-[5px] h-[50px]`}
+          ></div>
+          <div
+            className={`absolute -bottom-6 left-6 rotate-90 ${cornerColor} w-[5px] h-[50px]`}
+          ></div>
+        </>
+      )}
+
+      {/* bottom right corner */}
+      {showCorners && (
+        <>
+          <div
+            className={`absolute bottom-0 right-0 ${cornerColor} w-[5px] h-[50px]`}
+          ></div>
+          <div
+            className={`absolute -bottom-6 right-6 rotate-90 ${cornerColor} w-[5px] h-[50px]`}
+          ></div>
+        </>
+      )}
+
+      {children}
+    </div>
+  );
+};
 
 export default TemplateCorners;
