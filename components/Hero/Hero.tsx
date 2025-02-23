024cda432c3f8c42e35837edf579b3ee6b1316cb
Josh <jsbusiness305@gmail.com>
2025-02-22 22:21:28 -0500

    updated footer and hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero: React.FC = () => {
     return (
-        <div className="relative flex flex-col items-center justify-center h-[25vh] hero-img ">
+        <div className="relative flex flex-col items-center justify-center min-h-screen">
             <div className="relative p-5 w-[fit]  flex flex-col items-center justify-center">
                 <div className="absolute inset-0 bg-gradient-to-r from-[#7e50e9] via-[#9b59b6] to-[#2980b9] z-10 btn-layout"></div>
                 <h3 className="z-20 sm:text-4xl md:text-6xl lg:text-7xl font-bold " style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Hacks</h3>
