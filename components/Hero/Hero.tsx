ed47d9941823a40e64d92210e607467a3f5a2b46
Josh <jsbusiness305@gmail.com>
2025-03-06 15:03:44 -0500

    updated Hero component

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,20 +1,20 @@
 import { alkeron } from "@/public/fonts/fonts";
-// import HeroImage from "@/app/assets/img/hero.png";
-// import Image from "next/image";
+import HeroImage from "@/app/assets/img/hero.png";
+import Image from "next/image";
 const Hero: React.FC = () => {
   const heroText = ["Recode.", "Recharge.", "Redefine."];
 
   return (
     <div className="relative flex flex-col justify-center min-h-screen mt-[5rem]">
       <div className="absolute top-10 left-0 w-[100%] h-[0.15rem] bg-yellow-400 z-[-1]"></div>
-      {/* <div className="absolute top-0 right-0 z-[-1] bg-yellow-500 w-[5rem] h-[120rem]"></div> */}
+      <div className="absolute top-0 right-0 z-[-1] bg-yellow-500 w-[5rem] h-[120rem]"></div>
 
-      {/* <Image
+      <Image
         src={HeroImage}
         alt="Hero"
-        width={500}
+        width={900}
         className="absolute md:block top-1/2 right-10 transform  -translate-y-1/2 z-[-1]"
-      /> */}
+      />
 
       <div className="p-5 w-[fit] flex flex-col  justify-center ">
         {heroText.map((text: string, index: number) => {
